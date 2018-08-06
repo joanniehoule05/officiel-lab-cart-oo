d31f9168e08a09292461a4f91a6a27fc7c6f2d44
Jean Lalande <jeanlalande@gmail.com>
2018-08-06 11:26:47 -0400

    Extract XML repository from application service

--- src/main/java/ca/ulaval/glo4002/cart/application/CartApplicationService.java
+++ src/main/java/ca/ulaval/glo4002/cart/application/CartApplicationService.java
@@ -1,28 +1,21 @@
 package ca.ulaval.glo4002.cart.application;
 
-import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 
 import ca.ulaval.glo4002.cart.domain.cart.Cart;
 import ca.ulaval.glo4002.cart.domain.cart.CartItem;
 import ca.ulaval.glo4002.cart.domain.shop.ShopItem;
 
 public class CartApplicationService {
-	private static final String CART_STORAGE = "cart-storage";
 
-	private static File storageFile;
+	private CartRepository cartRepository;
 
-	static {
-		storageFile = XmlUtils.createStorageFile(CART_STORAGE);
+	public CartApplicationService() {
+		this.cartRepository = new CartRepository();
 	}
 
 	public Cart findOrCreateCartForClient(String email) {
-		List<Cart> carts = readCartsFromFile();
+		List<Cart> carts = cartRepository.retrieveCarts();
 
 		Cart cart = getCartByOwner(email, carts);
 
@@ -30,43 +23,24 @@ public class CartApplicationService {
 	}
 
 	public void addItemToCart(String email, ShopItem item) {
-		List<Cart> carts = readCartsFromFile();
+		List<Cart> carts = cartRepository.retrieveCarts();
 		Cart cart = getCartByOwner(email, carts);
 
 		cart.addItem(new CartItem(item.getName(), 1));
 
-		persistCarts(carts);
+		cartRepository.persistCarts(carts);
 	}
 
 	public List<Cart> findAllCarts() {
-		return this.readCartsFromFile();
+		return cartRepository.retrieveCarts();
 	}
 
 	private Cart getCartByOwner(String email, List<Cart> carts) {
 		return carts.stream().filter(c -> c.ownerEmail.equals(email)).findFirst().orElseGet(() -> {
 			Cart newCart = new Cart(email);
 			carts.add(newCart);
-			persistCarts(carts);
+			cartRepository.persistCarts(carts);
 			return newCart;
 		});
 	}
-
-	private void persistCarts(List<Cart> carts) {
-		Marshaller marshaller = XmlUtils.createMarshaller();
-		try {
-			marshaller.marshal(new CartList(carts), storageFile);
-		} catch (JAXBException e) {
-			throw new PersistenceException(e);
-		}
-	}
-
-	private List<Cart> readCartsFromFile() {
-		Unmarshaller unmarshaller = XmlUtils.createUnmarshaller();
-		try {
-			return ((CartList) unmarshaller.unmarshal(storageFile)).getCarts();
-		} catch (JAXBException e) {
-			return new ArrayList<>();
-		}
-	}
-
 }
---
+++ src/main/java/ca/ulaval/glo4002/cart/application/CartRepository.java
@@ -0,0 +1,41 @@
+package ca.ulaval.glo4002.cart.application;
+
+import ca.ulaval.glo4002.cart.application.CartList;
+import ca.ulaval.glo4002.cart.application.PersistenceException;
+import ca.ulaval.glo4002.cart.application.XmlUtils;
+import ca.ulaval.glo4002.cart.domain.cart.Cart;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CartRepository {
+    private static final String CART_STORAGE = "cart-storage";
+
+    private static File storageFile;
+
+    static {
+        storageFile = XmlUtils.createStorageFile(CART_STORAGE);
+    }
+
+    public List<Cart> retrieveCarts() {
+        Unmarshaller unmarshaller = XmlUtils.createUnmarshaller();
+        try {
+            return ((CartList) unmarshaller.unmarshal(storageFile)).getCarts();
+        } catch (JAXBException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public void persistCarts(List<Cart> carts) {
+        Marshaller marshaller = XmlUtils.createMarshaller();
+        try {
+            marshaller.marshal(new CartList(carts), storageFile);
+        } catch (JAXBException e) {
+            throw new PersistenceException(e);
+        }
+    }
+}
