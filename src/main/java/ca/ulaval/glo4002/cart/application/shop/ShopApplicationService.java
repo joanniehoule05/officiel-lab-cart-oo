ad06128f9611955c7881357c1b159d1e9ee60565
Jni <jni.viens@gmail.com>
2018-09-18 23:56:39 -0400

    Adds the price and weight to items in order to calculate shipping

--- src/main/java/ca/ulaval/glo4002/cart/application/shop/ShopApplicationService.java
+++ src/main/java/ca/ulaval/glo4002/cart/application/shop/ShopApplicationService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+import ca.ulaval.glo4002.cart.domain.shop.PrimeShopItem;
 import ca.ulaval.glo4002.cart.domain.shop.ShopItem;
+import ca.ulaval.glo4002.cart.domain.shop.StandardShopItem;
 
 public class ShopApplicationService {
     private final ShopRepository shopRepository;
@@ -32,17 +34,17 @@ public class ShopApplicationService {
     }
 
     private void prefillDemoData() {
-        addItem("1251521", "Peanuts", 1.20, true);
-        addItem("236637", "Clean Code", 0.50, false);
-        addItem("235265", "Détendeur Mares Abyss Navy 22", 0.15, true);
-        addItem("276101", "Imprimante 3D", 0.60, true);
-        addItem("818113", "GoPro", 4.60, true);
-        addItem("51-153", "Peinture à numéro", 1.40, true);
+        addItem(new StandardShopItem("1251521", "Peanuts", 5, 1, 1.20, true));
+        addItem(new PrimeShopItem("236637", "Clean Code", 35, 2, 0.50, false));
+        addItem(new StandardShopItem("235265", "Détendeur Mares Abyss Navy 22", 999, 5, 0.15, true));
+        addItem(new StandardShopItem("276101", "Imprimante 3D", 2341, 31, 0.60, true));
+        addItem(new PrimeShopItem("818113", "GoPro", 650, 1, 4.60, true));
+        addItem(new StandardShopItem("51-153", "Peinture à numéro", 1, 2, 1.40, true));
     }
 
-    private void addItem(String sku, String name, double margin, boolean available) {
+    private void addItem(ShopItem item) {
         List<ShopItem> items = shopRepository.readShopFromFile();
-        items.add(new ShopItem(sku, name, margin, available));
+        items.add(item);
 
         shopRepository.persistShop(items);
     }
