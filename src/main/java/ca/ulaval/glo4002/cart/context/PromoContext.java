77adbd1d38b6c70ec4e73450cfaf02c18d70d37c
Joannie Houle <joanniehoule05@gmail.com>
2018-10-20 15:10:31 -0400

    Début du défit 3m

---
+++ src/main/java/ca/ulaval/glo4002/cart/context/PromoContext.java
@@ -0,0 +1,12 @@
+package ca.ulaval.glo4002.cart.context;
+
+import ca.ulaval.glo4002.cart.application.cart.ServiceLocator;
+import ca.ulaval.glo4002.cart.domain.cart.CartItemFactory;
+import ca.ulaval.glo4002.cart.domain.cart.PromoCartItemFactory;
+
+public class PromoContext {
+	
+	  public void apply() {
+	        ServiceLocator.INSTANCE.register(CartItemFactory.class, new PromoCartItemFactory());
+	  }
+}
